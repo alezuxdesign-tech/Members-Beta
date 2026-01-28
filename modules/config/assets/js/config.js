bd6749eb5aee44b79ffe543a811f693b80db2565
alezuxdesign-tech <alezuxdesign@gmail.com>
2026-01-28 17:33:10 -0500

    + config

--- modules/config/assets/js/config.js
+++ modules/config/assets/js/config.js
@@ -1,20 +1,21 @@
 jQuery(document).ready(function ($) {
     // Escuchar clic en el encabezado del widget de configuración
     $(document).on('click', '.alezux-config-header', function (e) {
+        e.preventDefault();
         e.stopPropagation();
         var $wrapper = $(this).closest('.alezux-config-card');
 
         // Toggle state
-        $wrapper.toggleClass('is-open');
+        $wrapper.toggleClass('active');
 
         // Close other open widgets if any
-        $('.alezux-config-card').not($wrapper).removeClass('is-open');
+        $('.alezux-config-card').not($wrapper).removeClass('active');
     });
 
     // Close on click outside
     $(document).on('click', function (e) {
         if (!$(e.target).closest('.alezux-config-card').length) {
-            $('.alezux-config-card').removeClass('is-open');
+            $('.alezux-config-card').removeClass('active');
         }
     });
 });
