24174759935f48ccef9d74efb447221340cde17a
alezuxdesign-tech <alezuxdesign@gmail.com>
2026-01-28 10:27:49 -0500

    Update notifications.js

--- modules/notifications/assets/js/notifications.js
+++ modules/notifications/assets/js/notifications.js
@@ -110,9 +110,11 @@ jQuery(document).ready(function ($) {
         notifications.forEach(notif => {
             const isUnread = notif.is_read == '0' ? 'unread' : '';
             // Si hay avatar_url usamos img, si no, un icono por defecto
+            const bellIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>`;
+
             const avatar = notif.avatar_url
                 ? `<img src="${notif.avatar_url}" class="notif-avatar">`
-                : `<div class="notif-avatar"><i class="eicon-bell-o"></i></div>`;
+                : `<div class="notif-avatar">${bellIcon}</div>`;
 
             const html = `
                 <div class="alezux-notification-item ${isUnread}" data-id="${notif.id}" data-link="${notif.link}">
