363de2436650ae8675cdc306877d89052c32d5b6
alezuxdesign-tech <alezuxdesign@gmail.com>
2026-03-11 23:47:13 -0500

    Update Proyectos_Agencia.php

--- modules/proyectos-agencia/Proyectos_Agencia.php
+++ modules/proyectos-agencia/Proyectos_Agencia.php
@@ -63,19 +63,17 @@ class Proyectos_Agencia extends Module_Base {
     }
     
     public function enqueue_scripts() {
-        // Enqueue WP Media for Admin
-        if ( current_user_can( 'edit_posts' ) ) {
-            wp_enqueue_media();
-        }
+        // Enqueue WP Media no es necesario si usamos selector nativo
+        // wp_enqueue_media();
 
         // CSS
-        wp_register_style( 'alezux-kanban-css', ALEZUX_PROYECTOS_AGENCIA_URL . 'assets/css/kanban.css', [], '1.1.7' );
-        wp_register_style( 'alezux-client-view-css', ALEZUX_PROYECTOS_AGENCIA_URL . 'assets/css/client-view.css', [], '1.1.7' );
+        wp_register_style( 'alezux-kanban-css', ALEZUX_PROYECTOS_AGENCIA_URL . 'assets/css/kanban.css', [], '1.1.8' );
+        wp_register_style( 'alezux-client-view-css', ALEZUX_PROYECTOS_AGENCIA_URL . 'assets/css/client-view.css', [], '1.1.8' );
         // jQuery UI CSS for Datepicker
         wp_register_style( 'jquery-ui', 'https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css', [], '1.12.1' );
         
         // JS - Admin
-        wp_register_script( 'alezux-kanban-js', ALEZUX_PROYECTOS_AGENCIA_URL . 'assets/js/kanban-app.js', ['jquery', 'jquery-ui-sortable', 'jquery-ui-datepicker', 'jquery-ui-resizable'], '1.1.7', true );
+        wp_register_script( 'alezux-kanban-js', ALEZUX_PROYECTOS_AGENCIA_URL . 'assets/js/kanban-app.js', ['jquery', 'jquery-ui-sortable', 'jquery-ui-datepicker', 'jquery-ui-resizable'], '1.1.8', true );
         
         wp_localize_script( 'alezux-kanban-js', 'alezux_agency_vars', [
             'ajax_url' => admin_url( 'admin-ajax.php' ),
@@ -106,8 +104,7 @@ class Proyectos_Agencia extends Module_Base {
         // But since I can't easily detect that before render in all cases:
         // Admin Widget CSS/JS -> Only for Admins?
         if ( current_user_can( 'edit_posts' ) ) {
-             // wp_enqueue_style( 'alezux-kanban-css' ); // Actually better to let the widget depend on it
-             wp_enqueue_media();
+             // wp_enqueue_media(); 
         }
         
     }
