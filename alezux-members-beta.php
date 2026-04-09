f53c1c16597a5018654c3bff0916999337e22384
alezuxdesign-tech <alezuxdesign@gmail.com>
2026-04-09 11:26:59 -0500

    Update alezux-members-beta.php

--- alezux-members-beta.php
+++ alezux-members-beta.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Alezux Members Beta
  * Description: Plugin modular "Lego-style" para funcionalidades robustas y escalables.
- * Version: 1.0.8
+ * Version: 1.0.81
  * Author: Alezux
  * Text Domain: alezux-members
  */
